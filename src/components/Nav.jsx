7b9ec972821f12cb250b003822f90e247fc910fa
Sathwik Dasari <adityasathwikddv1772003@gmail.com>
2023-09-04 12:27:52 +0530

    commited

---
+++ src/components/Nav.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../components/Nav.css'
+
+const Nav = () => {
+  return (
+    
+      <>
+<div className="container">
+      <div className="pasword">
+        <div className="heading">
+          <h1>rafcsssssss</h1>
+        </div>
+        <div className="lengthbar">
+          <h3>Character length</h3>
+        </div>
+        <div className="checkboxes">
+
+        </div>
+        <div className="strength">
+
+        </div>
+          <div className="btn">
+              <button> Genarate</button>
+            </div>
+      </div>
+      </div>
+  
+    </>
+
+  )
+}
+
+export default Nav
